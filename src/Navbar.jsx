3d43b5c456e4f6ab95e6b6a55cb3abcecd6ee848
jahan241-35-202-svg <jahan241-35-202@diu.edu.bd>
2026-08-07 04:58:43 +0600

    Navbar.jsx

---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>My Portfolio</h2>
+
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
